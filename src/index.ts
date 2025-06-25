88474d3af931011678ea57a97475c41c5885fa02
Sahil Tiwaskar <sahiltiwaskar.21@stvincentngp.edu.in>
2025-06-25 14:41:56 +0530

    basic server setup

---
+++ src/index.ts
@@ -0,0 +1,22 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import router from "./routes";
+
+dotenv.config({path: ".env"});
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(express.json());
+app.use(cors())
+
+app.use("/api/v1", router);
+
+app.get("/", (req, res) => {
+  res.send("Well, This is a worker server.");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
