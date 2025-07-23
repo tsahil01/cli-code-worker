aa3035b283a46fe3bc016a9cda380f1450a98557
Sahil Tiwaskar <sahiltiwaskar.21@stvincentngp.edu.in>
2025-07-23 22:34:42 +0530

    Set JSON request body size limit to 100mb for express middleware

--- src/index.ts
+++ src/index.ts
@@ -11,7 +11,9 @@ export const geminiAPIKey = process.env.GEMINI_API_KEY;
 
 const app = express();
 
-app.use(express.json());
+app.use(express.json({
+  limit: "100mb",
+}));
 app.use(cors())
 
 app.use("/api/v1", router);
